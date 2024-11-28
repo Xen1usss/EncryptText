26ad0e7e2258834716f6854ec452dde62b446ca6
Ksenia <130775320+Xen1usss@users.noreply.github.com>
2024-11-28 15:04:11 +0300

    add new fragment to system of navigation

--- app/src/main/java/ks/encrypttext/fragments/TextFragment.kt
+++ app/src/main/java/ks/encrypttext/fragments/TextFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 import ks.encrypttext.R
 
 class TextFragment : Fragment() {
@@ -13,8 +15,15 @@ class TextFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_text, container, false)
+        val view = inflater.inflate(R.layout.fragment_text, container, false)
+        val encryptButton = view.findViewById<Button>(R.id.encryptButton)
+
+        encryptButton.setOnClickListener {
+            val navController = view.findNavController()
+            navController.navigate(R.id.action_textFragment_to_encrypt)
+        }
+
+        return view
     }
 
 }
